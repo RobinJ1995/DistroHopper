cab115579dbdc678b83a7ed84ac78d36bc0db87d
= <robin@robinj.be>
2014-11-02 13:02:08 +0100

    gradle update

--- UbuntuLauncher/colormixer/build/generated/source/buildConfig/test/debug/com/commonsware/cwac/colormixer/test/BuildConfig.java
+++ UbuntuLauncher/colormixer/build/generated/source/buildConfig/test/debug/com/commonsware/cwac/colormixer/test/BuildConfig.java
@@ -5,9 +5,14 @@ package com.commonsware.cwac.colormixer.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.commonsware.cwac.colormixer.test";
+  public static final String APPLICATION_ID = "com.commonsware.cwac.colormixer.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = -1;
   public static final String VERSION_NAME = "";
+  /**
+   * @deprecated Use {@link #APPLICATION_ID}
+   */
+  @Deprecated
+  public static final String PACKAGE_NAME = "com.commonsware.cwac.colormixer.test";
 }
