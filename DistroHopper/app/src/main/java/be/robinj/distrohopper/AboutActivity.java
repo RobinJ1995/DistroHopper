0f3612358c686c1c2259aa7a4126a631055c24a8
Robin Jacobs <robin@robinj.be>
2017-03-14 21:20:01 +0000

    A little easter egg for nostalgia's sake

--- DistroHopper/app/src/main/java/be/robinj/distrohopper/AboutActivity.java
+++ DistroHopper/app/src/main/java/be/robinj/distrohopper/AboutActivity.java
@@ -1,14 +1,20 @@
 package be.robinj.distrohopper;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import be.robinj.distrohopper.dev.Log;
 
 
 public class AboutActivity extends Activity
@@ -28,6 +34,7 @@ public class AboutActivity extends Activity
 			TextView tvDevUrl = (TextView) this.findViewById (R.id.tvDevUrl);
 			TextView tvDevEmail = (TextView) this.findViewById (R.id.tvDevEmail);
 			TextView tvVersion = (TextView) this.findViewById (R.id.tvVersion);
+			ImageView ivLogo = (ImageView) this.findViewById (R.id.ivLogo);
 
 			tvDevUrl.setText (Html.fromHtml ("<a href=\"http://robinj.be/\">RobinJ.be</a>"));
 			tvDevEmail.setText (Html.fromHtml ("<a href=\"mailto:android-dev@robinj.be\">android-dev@robinj.be</a>"));
@@ -35,6 +42,31 @@ public class AboutActivity extends Activity
 
 			tvDevUrl.setMovementMethod (LinkMovementMethod.getInstance ());
 			tvDevEmail.setMovementMethod (LinkMovementMethod.getInstance ());
+			
+			final Context context = this.getBaseContext ();
+			
+			ivLogo.setOnClickListener
+			(
+				new View.OnClickListener ()
+				{
+					private short clicked = 0;
+					private MediaPlayer player;
+					
+					@Override
+					public void onClick (View view)
+					{
+						if (++this.clicked % 3 == 0)
+						{
+							if (this.player == null)
+								this.player = MediaPlayer.create (context, R.raw.ubuntu);
+							else
+								this.player.seekTo (0);
+							
+							player.start ();
+						}
+					}
+				}
+			);
 		}
 		catch (Exception ex)
 		{
