fdec2f250d8aa09ea8e60adefc5c86b934657b2f
Robin Jacobs <robin@robinj.be>
2017-03-30 10:24:52 +0100

    Got rid of Proguard because it just fucks up too many things...

--- DistroHopper/app/src/main/java/be/robinj/distrohopper/HomeActivity.java
+++ DistroHopper/app/src/main/java/be/robinj/distrohopper/HomeActivity.java
@@ -422,14 +422,6 @@ public class HomeActivity extends AppCompatActivity
 	@Override
 	public boolean onOptionsItemSelected (MenuItem item)
 	{
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId ();
-		/*if (id == R.id.action_settings)
-		{
-			return true;
-		}*/
 		return super.onOptionsItemSelected (item);
 	}
 
