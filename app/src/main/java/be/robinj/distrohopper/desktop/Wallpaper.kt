f52afa5da58a394215a23819d18541a5e80b8d8c
Robin Jacobs <robin@robinj.be>
2025-10-14 14:51:44 +0100

    changed Wallpaper class from java to kotlin

--- app/src/main/java/be/robinj/distrohopper/desktop/Wallpaper.kt
+++ app/src/main/java/be/robinj/distrohopper/desktop/Wallpaper.kt
@@ -1,216 +1,197 @@
-package be.robinj.distrohopper.desktop;
+package be.robinj.distrohopper.desktop
 
-import android.Manifest;
-import android.app.WallpaperInfo;
-import android.app.WallpaperManager;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-
-import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
-
-import android.os.Build;
-import android.util.AttributeSet;
-import android.widget.ImageView;
-
-import be.robinj.distrohopper.ExceptionHandler;
-import be.robinj.distrohopper.Image;
-import be.robinj.distrohopper.Permission;
-import be.robinj.distrohopper.R;
-import be.robinj.distrohopper.dev.Log;
-import be.robinj.distrohopper.preferences.Preference;
-import be.robinj.distrohopper.preferences.Preferences;
+import android.Manifest
+import android.app.WallpaperManager
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.util.AttributeSet
+import android.widget.ImageView
+import be.robinj.distrohopper.ExceptionHandler
+import be.robinj.distrohopper.Image
+import be.robinj.distrohopper.Permission
+import be.robinj.distrohopper.R
+import be.robinj.distrohopper.dev.Log
+import be.robinj.distrohopper.preferences.Preference
+import be.robinj.distrohopper.preferences.Preferences
+import androidx.core.graphics.scale
 
 /**
  * Created by robin on 8/21/14.
  */
-public class Wallpaper extends ImageView {
-	private static final int COLOUR_UBUNTU_ORANGE = Color.rgb(180, 60 ,18);
-	private static final Log LOG = Log.getInstance();
+class Wallpaper : ImageView {
+    private val context: Context
+    private var img: Drawable? = null
+    private var blurred: Drawable? = null
+    private var mode: String? = null
 
-	private Context context;
-	@Nullable private Drawable img;
-	@Nullable private Drawable blurred;
-	private String mode;
+    var isLiveWallpaper: Boolean = false
+        private set
 
-	private boolean liveWallpaper = false;
+    constructor(context: Context) : super(context) {
+        this.context = context
+    }
 
-	public Wallpaper (Context context)
-	{
-		super (context);
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        this.context = context
+    }
 
-		this.context = context;
-	}
+    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        this.context = context
+    }
 
-	public Wallpaper (Context context, AttributeSet attrs)
-	{
-		super (context, attrs);
+    fun init() {
+        val wpman = WallpaperManager.getInstance(this.context)
+        val permissionExternalStorage =
+            Permission(this.context, Manifest.permission.READ_EXTERNAL_STORAGE)
 
-		this.context = context;
-	}
-
-	public Wallpaper (Context context, AttributeSet attrs, int defStyle)
-	{
-		super (context, attrs, defStyle);
-
-		this.context = context;
-	}
-
-	public void init ()
-	{
-		final WallpaperManager wpman = WallpaperManager.getInstance(this.context);
-		final Permission permissionExternalStorage = new Permission(this.context, Manifest.permission.READ_EXTERNAL_STORAGE);
-
-		if (permissionExternalStorage.check()) {
-			LOG.i("Wallpaper", "READ_EXTERNAL_STORAGE permission granted. Trying to obtain and blur wallpaper...");
-			try
-			{
-				/*
+        if (permissionExternalStorage.check()) {
+            LOG.i(
+                "Wallpaper",
+                "READ_EXTERNAL_STORAGE permission granted. Trying to obtain and blur wallpaper..."
+            )
+            try {
+                /*
 				 * This will never succeed on Android 13, as Google in their wisdom deprecated the
 				 * READ_EXTERNAL_STORAGE permission, but still requires it to get obtain the user
 				 * wallpaper.
 				 * Very useful for home screen replacements like these.
 				 */
-				this.img = wpman.getDrawable();
+                this.img = wpman.getDrawable()
 
-				//TODO// Huge memory hog! Need to get rid of this. //
-				SharedPreferences prefs = Preferences.getSharedPreferences(this.context, Preferences.PREFERENCES);
-				this.mode = prefs.getString (Preference.WALLPAPER_BLUR_MODE.getName(), "darken");
+                //TODO// Huge memory hog! Need to get rid of this. //
+                val prefs = Preferences.getSharedPreferences(this.context, Preferences.PREFERENCES)
+                this.mode = prefs.getString(Preference.WALLPAPER_BLUR_MODE.getName(), "darken")
 
-				if (mode.equals ("scale"))
-				{
-					Drawable blurred = wpman.getDrawable ();
+                if (mode == "scale") {
+                    val blurred = wpman.getDrawable()
 
-					BitmapDrawable bmdBlurred = (BitmapDrawable) blurred;
-					Bitmap bmBlurred = bmdBlurred.getBitmap ();
+                    var bmdBlurred = blurred as BitmapDrawable?
+                    var bmBlurred = bmdBlurred!!.bitmap
 
-					float origWidth = bmBlurred.getWidth ();
-					float origHeight = bmBlurred.getHeight ();
+                    val origWidth = bmBlurred.width.toFloat()
+                    val origHeight = bmBlurred.height.toFloat()
 
-					int width = 200;
-					int height = (int) (origHeight * (200F / origWidth));
+                    val width = 200
+                    val height = (origHeight * (200f / origWidth)).toInt()
 
-					bmBlurred = Bitmap.createScaledBitmap (bmBlurred, width, height, true);
-					bmBlurred = Bitmap.createScaledBitmap (bmBlurred, (int) origWidth, (int) origHeight, true);
+                    bmBlurred = bmBlurred.scale(width, height)
+                        .scale(origWidth.toInt(), origHeight.toInt())
 
-					bmdBlurred = new BitmapDrawable (bmBlurred);
-					this.blurred = bmdBlurred;
-				}
-			}
-			catch (OutOfMemoryError ex) // I'd prefer the image not being blurred over the app crashing //
-			{
-				this.img = null;
-				this.blurred = null;
+                    bmdBlurred = BitmapDrawable(bmBlurred)
+                    this.blurred = bmdBlurred
+                }
+            } catch (ex: OutOfMemoryError) // I'd prefer the image not being blurred over the app crashing //
+            {
+                this.img = null
+                this.blurred = null
 
-				new ExceptionHandler(ex).logAndTrack();
-			}
-		} else {
-			LOG.i("Wallpaper", "READ_EXTERNAL_STORAGE permission not granted or Android version >= 13.");
-			this.img = null;
-			this.blurred = null;
-		}
+                ExceptionHandler(ex).logAndTrack()
+            }
+        } else {
+            LOG.i(
+                "Wallpaper",
+                "READ_EXTERNAL_STORAGE permission not granted or Android version >= 13."
+            )
+            this.img = null
+            this.blurred = null
+        }
 
-		final WallpaperInfo info = wpman.getWallpaperInfo ();
-		this.liveWallpaper = (info != null
-				&& !info.getPackageName().startsWith("net.oneplus.launcher")); // OnePlus 5T always seems to use a live wallpaper, presumably for the blur animation when opening OnePlus' "shelf"
-	}
+        val info = wpman.wallpaperInfo
+        this.isLiveWallpaper = (info != null
+                && !info.packageName
+            .startsWith("net.oneplus.launcher")) // OnePlus 5T always seems to use a live wallpaper, presumably for the blur animation when opening OnePlus' "shelf"
+    }
 
-	public void set ()
-	{
-		if (this.img == null && this.blurred == null) {
-			return;
-		}
+    fun set() {
+        if (this.img == null && this.blurred == null) {
+            return
+        }
 
-		if (! this.mode.equals ("no"))
-		{
-			if (this.liveWallpaper || this.blurred == null || this.mode.equals ("darken"))
-			{
-				this.setImageDrawable (null);
-				this.setBackgroundColor (this.getResources ().getColor (R.color.transparent));
-			}
-			else
-			{
-				this.setImageDrawable (this.img);
-				this.setBackgroundDrawable (null); // setBackgroundDrawable is deprecated, but setBackground is unspported on older versions of Android //
-			}
-		}
-	}
+        if (this.mode != "no") {
+            if (this.isLiveWallpaper || this.blurred == null || this.mode == "darken") {
+                this.setImageDrawable(null)
+                this.setBackgroundColor(this.resources.getColor(R.color.transparent))
+            } else {
+                this.setImageDrawable(this.img)
+                this.setBackgroundDrawable(null) // setBackgroundDrawable is deprecated, but setBackground is unspported on older versions of Android //
+            }
+        }
+    }
 
-	public void blur ()
-	{
-		if (this.img == null && this.blurred == null) {
-			return;
-		}
+    fun blur() {
+        if (this.img == null && this.blurred == null) {
+            return
+        }
 
-		if (! this.mode.equals ("no"))
-		{
-			if (this.liveWallpaper || this.blurred == null || this.mode.equals ("darken"))
-			{
-				this.setImageDrawable (null);
-				this.setBackgroundColor (this.getResources ().getColor (R.color.transparentblack60));
-			}
-			else
-			{
-				this.setImageDrawable (this.blurred);
-				this.setBackgroundDrawable (null); // setBackgroundDrawable is deprecated, but setBackground is unspported on older versions of Android //
-			}
-		}
-	}
+        if (this.mode != "no") {
+            if (this.isLiveWallpaper || this.blurred == null || this.mode == "darken") {
+                this.setImageDrawable(null)
+                this.setBackgroundColor(this.resources.getColor(R.color.transparentblack60))
+            } else {
+                this.setImageDrawable(this.blurred)
+                this.setBackgroundDrawable(null) // setBackgroundDrawable is deprecated, but setBackground is unspported on older versions of Android //
+            }
+        }
+    }
 
-	public void unblur ()
-	{
-		if (this.img == null && this.blurred == null) {
-			return;
-		}
+    fun unblur() {
+        if (this.img == null && this.blurred == null) {
+            return
+        }
 
-		if (! this.mode.equals ("no"))
-		{
-			if (this.liveWallpaper || this.blurred == null || this.mode.equals ("darken"))
-			{
-				this.setImageDrawable (null);
-				this.setBackgroundColor (this.getResources ().getColor (R.color.transparent));
-			}
-			else
-			{
-				this.setImageDrawable (this.img);
-				this.setBackgroundDrawable (null); // setBackgroundDrawable is deprecated, but setBackground is unspported on older versions of Android //
-			}
-		}
-	}
+        if (this.mode != "no") {
+            if (this.isLiveWallpaper || this.blurred == null || this.mode == "darken") {
+                this.setImageDrawable(null)
+                this.setBackgroundColor(this.resources.getColor(R.color.transparent))
+            } else {
+                this.setImageDrawable(this.img)
+                this.setBackgroundDrawable(null) // setBackgroundDrawable is deprecated, but setBackground is unspported on older versions of Android //
+            }
+        }
+    }
 
-	public int getAverageColour(final int alpha)
-	{
-		try {
-			if (this.img != null) {
-				LOG.v("Wallpaper", "Calculating dominant colour of wallpaper...");
-				final Image image = new Image(this.img);
+    fun getAverageColour(alpha: Int): Int {
+        try {
+            if (this.img != null) {
+                LOG.v("Wallpaper", "Calculating dominant colour of wallpaper...")
+                val image = Image(this.img)
 
-				return image.getAverageColour(alpha);
-			} else if (Build.VERSION.SDK_INT >= 27) {
-				LOG.v("Wallpaper", "Trying to obtain primary wallpaper colour from Android...");
-				final WallpaperManager wpman = WallpaperManager.getInstance(this.context);
-				final Color primaryColour = wpman.getWallpaperColors(WallpaperManager.FLAG_SYSTEM).getPrimaryColor();
+                return image.getAverageColour(alpha)
+            } else if (Build.VERSION.SDK_INT >= 27) {
+                LOG.v("Wallpaper", "Trying to obtain primary wallpaper colour from Android...")
+                val wpman = WallpaperManager.getInstance(this.context)
+                val primaryColour =
+                    wpman.getWallpaperColors(WallpaperManager.FLAG_SYSTEM)!!.primaryColor
 
-				return Color.argb(alpha, primaryColour.red(), primaryColour.green(), primaryColour.blue());
-			}
-		} catch (final Exception ex) {
-			new ExceptionHandler(ex).logAndTrack();
-		}
+                return Color.argb(
+                    alpha.toFloat(),
+                    primaryColour.red(),
+                    primaryColour.green(),
+                    primaryColour.blue()
+                )
+            }
+        } catch (ex: Exception) {
+            ExceptionHandler(ex).logAndTrack()
+        }
 
-		/*
+        /*
 		 * What with Google crippling the APIs faster than they can invent new methods of achieving
 		 * similar results, this will have to do.
 		 */
-		LOG.v("Wallpaper", "Falling back to \"Ubuntu orange\" as dominant colour.");
-		return COLOUR_UBUNTU_ORANGE;
-	}
+        LOG.v("Wallpaper", "Falling back to \"Ubuntu orange\" as dominant colour.")
+        return COLOUR_UBUNTU_ORANGE
+    }
 
-	public boolean isLiveWallpaper ()
-	{
-		return this.liveWallpaper;
-	}
+    companion object {
+        private val COLOUR_UBUNTU_ORANGE = Color.rgb(180, 60, 18)
+        private val LOG: Log = Log.getInstance()
+    }
 }
